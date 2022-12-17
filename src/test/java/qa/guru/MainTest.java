e4a164b40f0007a3113b68a906cbc99e4996a148
Чиликин Олег Владимирович <ChilikinOV@gnivc.ru>
2022-12-17 18:26:36 +0300

    init 2

--- src/test/java/qa/guru/MainTest.java
+++ src/test/java/qa/guru/MainTest.java
@@ -1,4 +1,17 @@
 package qa.guru;
 
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
 public class MainTest {
+
+    @Test
+    void mainTest() {
+
+        open("https://github.com/");
+        $()
+
+    }
 }
